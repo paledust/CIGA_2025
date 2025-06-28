5c6437fdd0d046d766c800d05f5ea707655b15bd
kevzn <paledust@hotmail.com>
2025-06-29 01:40:34 +0800

    配置字体动态出现

--- Assets/Scripts/Equipment/Equipment_TEXT.cs
+++ Assets/Scripts/Equipment/Equipment_TEXT.cs
@@ -1,39 +1,19 @@
 using System.Collections;
+using Febucci.UI;
 using TMPro;
 using UnityEngine;
 
 public class Equipment_TEXT : Equipment
 {
-    [SerializeField] private TextMeshPro text;
-    [SerializeField] private float textMoveSpeed;
-    [SerializeField] private float textMoveStep;
-    [SerializeField] private float initTextPos = 4;
-    [SerializeField] private float recycleLength = 8;
+    [SerializeField] private TypewriterByCharacter typeWriter;
     private float textSize;
     private float moveTimer;
     public override void ProcessContent(LastWords_SO lastWords)
     {
         var words = lastWords as LastWords_TEXT;
-        text.text = words.GetShowingText();
-        text.transform.localPosition = Vector2.right * initTextPos;
-        StartCoroutine(coroutineGetBounds());
+        typeWriter.ShowText(words.GetShowingText());
         moveTimer = 0;
 
         base.ProcessContent(lastWords);
-    }
-    IEnumerator coroutineGetBounds()
-    {
-        yield return null;
-        textSize = text.textBounds.size.x;
-    }
-    void Update()
-    {
-        moveTimer += Time.deltaTime * textMoveSpeed;
-        float moveDist = Mathf.FloorToInt(moveTimer / textMoveStep) * textMoveStep;
-        text.transform.localPosition = Vector3.right * (-moveDist + initTextPos);
-        if (moveDist > textSize+recycleLength)
-        {
-            moveTimer = 0;
-        }
     }
 }
